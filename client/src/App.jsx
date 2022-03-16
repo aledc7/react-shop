ebc65b3c40dc5edfd9bfa4af37a85def62a14097
aledc7 <alejandrocdecastro@gmail.com>
2022-03-16 17:34:13 -0300

    Add Condition for register and login routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,28 +5,19 @@ import Product from './pages/Product/Product';
 import Cart from './pages/Cart/Cart';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+
 function App() {
+
+  const user = true;
   return (
-    <>
-
-
-
-
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/products/:category/' element={<ProductList/>} />
+        <Route path="/login" element={ user ? <Home/> : <Login />}/>
         <Route path='/products/:id/' element={<Product/>} />
         <Route path='/cart/' element={<Cart/>} />
-        <Route path='/login/' element={<Login/>} />
-        <Route path='/register/' element={<Register/>} />
-        
+        <Route path="/register" element={ user ? <Home/> : <Register />}/>
       </Routes>
-
-
-
-
-
-    </>
   );
 }
 export default App ;
