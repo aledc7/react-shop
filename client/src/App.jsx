21c47fafba1a981de472ee2499f06358d0ad8caa
aledc7 <alejandrocdecastro@gmail.com>
2022-03-16 16:01:35 -0300

    install react-router-dom and put the login, register, home, products routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
-// import Product from "./pages/Product/Product";
-// import Home from "./pages/Home/Home";
-// import ProductList from "./ProductList/ProductList";
-// import Register from "./pages/Register/Register";
-// import Login from "./pages/Login/Login";
-import Cart from "./pages/Cart/Cart";
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import ProductList from './ProductList/ProductList';
+import Product from './pages/Product/Product';
+import Cart from './pages/Cart/Cart';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+function App() {
+  return (
+    <>
 
-const App = () => {
-  return <Cart/>
-};
 
-export default App;
+
+
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/products/:category/' element={<ProductList/>} />
+        <Route path='/products/:id/' element={<Product/>} />
+        <Route path='/cart/' element={<Cart/>} />
+        <Route path='/login/' element={<Login/>} />
+        <Route path='/register/' element={<Register/>} />
+        
+      </Routes>
+
+
+
+
+
+    </>
+  );
+}
+export default App ;
